a74abc2b4a1d0a3bfd1ffa98d9de01d7e18bc2c0
stevo01 <stevo01@github.com>
2018-08-29 08:42:26 +0200

    kap2geojson: parameter -f supports for name of directory added

--- kap2geojson.py
+++ kap2geojson.py
@@ -11,6 +11,8 @@ from utils.convex_huell import convexhull
 import geojson
 from geojson.feature import Feature
 from geojson.geometry import Polygon
+from pathlib import Path
+from utils.filesystem import GetFileList
 
 # this script generates a geojson file for open sea map download layer
 
@@ -25,18 +27,38 @@ if __name__ == '__main__':
 
     parser.add_option("-u", "--url", type="string", help="server address", dest="url", default="https://ftp5.gwdg.de")
     parser.add_option("-d", "--dir", type="string", help="json file directory on server", dest="InPath", default="/pub/misc/openstreetmap/openseamap/charts/kap")
-    parser.add_option("-f", "--filename", type="string", help="filename of the kap file archive", dest="kapfilename", default="sample/7z/OSM-OpenCPN2-KAP-Adria-20180629-0337.7z")
+    parser.add_option("-f", "--filename", type="string", help="filename of the kap file archive", dest="archivfilename", default="./sample/7z/")
     parser.add_option("-p", "--project", type="string", help="filename of the chart bundler project file", dest="ProjectFile", default="sample/atlas/osmcb/sea/osmcb-catalog-Adria.xml")
     parser.add_option("-z", "--zoom", type="int", help="zoom level", dest="zoom", default=12)
 
     options, arguments = parser.parse_args()
 
+    # determine filename of 7z archive with kap data
+
+    # check if file exists
+    my_file = Path(options.archivfilename)
+    if my_file.is_file():
+        archivfilename = options.archivfilename
+    elif my_file.is_dir():
+        # search for file
+        print("parameter --filename points to valid directory".format(options.archivfilename))
+        filelist = GetFileList(options.archivfilename, _filter=".7z")
+        if len(filelist) is 1:
+            archivfilename = options.archivfilename+filelist[0]
+        else:
+            print("error invalid parameter for --filename detected 2: {}".format(options.archivfilename))
+            exit(1)
+    else:
+        print("error invalid parameter for --filename detected 1: {}".format(options.archivfilename))
+        exit(1)
+    # directory exists
+
     # get timestamp from filename of the kap file archive
-    date = options.kapfilename[-16:-3]
+    date = archivfilename[-16:-3]
     date = HandleDate(date)
 
     # get size ( from kap file archive )
-    filesize = os.path.getsize(options.kapfilename)
+    filesize = os.path.getsize(archivfilename)
 
     # get chart name ( from project file )
     atlas, mapname = ExtractMapsFromAtlas(options.ProjectFile)
@@ -56,12 +78,12 @@ if __name__ == '__main__':
     huell = convexhull(points)
     huell.append(huell[0])
 
-    outfilename = options.kapfilename[:-3] + '.geojson'
+    outfilename = archivfilename[:-3] + '.geojson'
 
     url = "{}{}/{}".format(
         options.url,
         options.InPath,
-        os.path.basename(options.kapfilename))
+        os.path.basename(archivfilename))
 
     # generate geojson object
     jsonres = Feature(geometry=Polygon([huell]),
