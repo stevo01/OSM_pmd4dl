25922c7986ceaad24078314dfd1f7d5399deed4c
stevo01 <stevo01@github.com>
2018-11-06 07:22:38 +0100

    FetchJsonFiles: default path changed

--- FetchJsonFiles.py
+++ FetchJsonFiles.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     atlas = list()
 
     parser.add_option("-a", "--address", type="string", help="server address", dest="address", default="ftp5.gwdg.de")
-    parser.add_option("-i", "--dir", type="string", help="json file directory on server", dest="InPath", default="/pub/misc/openstreetmap/openseamap/charts/kap")
+    parser.add_option("-i", "--dir", type="string", help="json file directory on server", dest="InPath", default="/pub/misc/openstreetmap/openseamap/charts/history/kap")
     parser.add_option("-o", "--filedir", type="string", help="url", dest="OutPath", default="sample/geojson/kap/")
 
     options, arguments = parser.parse_args()
